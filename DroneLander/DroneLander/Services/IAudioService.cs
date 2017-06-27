fe24eaa5c7e2dedc46efe7448d10c6bd084de3c6
Paulo Lopes <caixapvl@gmail.com>
2017-06-27 12:31:54 +0100

    Adicionar arquivos de projeto.

---
+++ DroneLander/DroneLander/Services/IAudioService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneLander.Services {
+    public interface IAudioService {
+        void AdjustVolume(double volume);
+        void KillEngine();
+        void ToggleEngine();
+        Action OnFinishedPlaying { get; set; }
+    }
+}
